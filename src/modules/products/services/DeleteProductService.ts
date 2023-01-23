78593ac122fa91ecf32ab0392607fc34b689a217
Lucas Henrique <contato@lucasweb.me>
2023-01-23 19:05:28 -0300

    Adicionado serviços e controllers do produto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequrest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequrest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 404);
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequrest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequrest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 404);
+    }
+    return product;
+  }
+}
+
+export default ShowProductService;
