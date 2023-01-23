035ac6021cd84f85d062e9fab12a7f2dde871022
Lucas Henrique <contato@lucasweb.me>
2023-01-23 15:40:38 -0300

    first commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import router from '@shared/routes';
+import cors from 'cors';
+import express, { NextFunction, Response, Request } from 'express';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Aplicação rodando na porta 3333!');
+});
